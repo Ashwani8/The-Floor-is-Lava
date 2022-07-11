cc32b7d8ef7edc213463267a18a2da47360f1a34
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-07-10 23:21:20 -0500

    add cylinder and make it jump

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    
     public string myMessage;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,12 @@ public class HelloWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(myMessage);
+        //Debug.Log(myMessage);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("spacebar was pressed");
+            transform.position += new Vector3(0, 1, 0);
+        }
+        
     }
 }
