655be7460bec9ad15e3356ff9db863d8c1e8471c
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-07-12 19:55:56 -0500

    smooth motion using get axix

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    
+    public float speed;
     public string myMessage;
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,19 @@ public class HelloWorld : MonoBehaviour
             print("spacebar was pressed");
             transform.position += new Vector3(0, 1, 0);
         }
-         
 
+        // Use GetAxis for Smooth motion instead of Key, left/Right arrow "Horizontal"/"Vertical" motion 
+        // will go smoothly from 0 to -1 or +1 value
+        float xInput = Input.GetAxis("Horizontal"); // we are storing
+        float yInput = Input.GetAxis("Vertical");
+        // we are using delta time to change x positon per second rather than frame speed time as different
+        // system may have different frame update speed
+        xInput = xInput * speed * Time.deltaTime;
+        yInput = yInput * speed * Time.deltaTime;
+        // Use Get Axis for Smooth motion, from 0 to 1
+        //print(xInput);
+        //print(yInput);
+        // will move object with arrow keys or ctrl +a or d, up shipt+w or s
+        transform.Translate(xInput, yInput, 0);
     }
 }
